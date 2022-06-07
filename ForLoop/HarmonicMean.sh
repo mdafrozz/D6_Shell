c82221b6d06d5d5e75b826946da5fc7712bcb2d9
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 21:29:33 +0530

    Program to find harmonic mean

---
+++ ForLoop/HarmonicMean.sh
@@ -0,0 +1,10 @@
+echo "program to take command line argument n and print nth harmonic number"
+read -p "enter the number" num
+h1=0
+
+for ((i=1;i<=num;i++))
+do
+temp=$(( 1/$i ))
+h1=$(($h1+$temp))
+done
+echo "harmonic number is $h1"
